5b5cc28a3f5f418f4f6941c4b2cc249f9b3a7793
repplix <116941865+repplix@users.noreply.github.com>
2023-10-17 17:29:49 +0200

    Added first prototype for HTML receiving

---
+++ src/main/java/io/jexxa/jlegmed/plugins/html/producer/HTMLProducer.java
@@ -0,0 +1,28 @@
+package io.jexxa.jlegmed.plugins.html.producer;
+
+import io.jexxa.jlegmed.core.flowgraph.Context;
+import io.jexxa.jlegmed.core.producer.Producer;
+import kong.unirest.Unirest;
+
+public class HTMLProducer implements Producer {
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String APPLICATION_TYPE = "application/json";
+
+    private final String url;
+
+    public HTMLProducer(String url)
+    {
+        this.url = url;
+    }
+
+    @Override
+    public Object produce(Class<?> clazz, Context context) {
+
+        var result = Unirest.get(url)
+                .header(CONTENT_TYPE, APPLICATION_TYPE)
+                .asObject(clazz).getBody();
+
+        System.out.println(result);
+        return result;
+    }
+}
---
+++ src/test/java/io/jexxa/jlegmed/plugins/html/HTMLReaderTest.java
@@ -0,0 +1,65 @@
+package io.jexxa.jlegmed.plugins.html;
+
+import io.jexxa.jlegmed.core.JLegMed;
+import io.jexxa.jlegmed.core.VersionInfo;
+import io.jexxa.jlegmed.core.flowgraph.AbstractFlowGraph;
+import io.jexxa.jlegmed.core.flowgraph.FlowGraph;
+import io.jexxa.jlegmed.core.producer.Producer;
+import io.jexxa.jlegmed.core.producer.ProducerURL;
+import io.jexxa.jlegmed.plugins.generic.MessageCollector;
+import io.jexxa.jlegmed.plugins.generic.processor.GenericProcessors;
+import io.jexxa.jlegmed.plugins.html.producer.HTMLProducer;
+import org.junit.jupiter.api.Test;
+
+import static io.jexxa.jlegmed.plugins.html.HTMLReaderTest.HTMLReaderURL.httpURL;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+class HTMLReaderTest {
+    @Test
+    void testFlowGraph() {
+        //Arrange
+        var messageCollector = new MessageCollector<VersionInfo>();
+        var jlegmed = new JLegMed();
+        jlegmed.newFlowGraph("HTMLReader")
+                .each(50, MILLISECONDS)
+                //.receive(VersionInfo.class).from(httpURL("http://localhost:7503/BoundedContext/contextVersion")).asJson()
+                .receive(VersionInfo.class).generatedWith(() -> new VersionInfo("a","b", "s", "d" ))
+                .andProcessWith( GenericProcessors::idProcessor )
+                .andProcessWith( GenericProcessors::consoleLogger )
+                .andProcessWith( messageCollector::collect);
+
+        //Act
+        jlegmed.start();
+
+        //Assert
+        await().atMost(3, SECONDS).until(() -> messageCollector.getNumberOfReceivedMessages() >= 3);
+        jlegmed.stop();
+    }
+
+    public static class HTMLReaderURL implements ProducerURL {
+
+        private final String url;
+        private FlowGraph flowGraph;
+
+        public HTMLReaderURL(String url)
+        {
+            this.url = url;
+        }
+        @Override
+        public <T> Producer init(AbstractFlowGraph<T> flowGraph) {
+            this.flowGraph = flowGraph;
+            return new HTMLProducer(url);
+        }
+
+        public FlowGraph asJson()
+        {
+            return flowGraph;
+        }
+        public static HTMLReaderURL httpURL(String url)
+        {
+            return new HTMLReaderURL(url);
+        }
+    }
+}

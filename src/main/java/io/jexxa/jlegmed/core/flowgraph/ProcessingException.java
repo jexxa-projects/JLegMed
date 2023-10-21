9b06e69cc0dedab6b2a470bd45d27c3646f11e13
repplix <116941865+repplix@users.noreply.github.com>
2023-10-21 07:14:03 +0200

    Cleanup warnings

--- src/main/java/io/jexxa/jlegmed/core/flowgraph/ProcessingException.java
+++ src/main/java/io/jexxa/jlegmed/core/flowgraph/ProcessingException.java
@@ -1,12 +1,12 @@
 package io.jexxa.jlegmed.core.flowgraph;
 
 public class ProcessingException extends RuntimeException {
-    public ProcessingException(AbstractFlowGraph flowGraph, Content data, Throwable throwable)
+    public ProcessingException(AbstractFlowGraph<?> flowGraph, Content data, Throwable throwable)
     {
         super(getMessage(flowGraph, data, throwable), throwable);
     }
 
-    private static String getMessage(AbstractFlowGraph flowGraph, Content data, Throwable throwable)
+    private static String getMessage(AbstractFlowGraph<?> flowGraph, Content data, Throwable throwable)
     {
         if (throwable instanceof ClassCastException ) {
             return throwable.getClass().getSimpleName() + " occurred in FlowGraph " + flowGraph.getFlowGraphID()

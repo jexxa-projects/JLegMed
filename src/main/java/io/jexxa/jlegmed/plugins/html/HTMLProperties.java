7cddb30645dd414d549a187ad3f1eebe4817090d
repplix <116941865+repplix@users.noreply.github.com>
2023-10-28 10:11:13 +0200

    Change: + Added properties for HTML-requests to set a proxy + Each filter instance now uses its own unirest instance to enable different configurations and proper cleanup per instance

---
+++ src/main/java/io/jexxa/jlegmed/plugins/html/HTMLProperties.java
@@ -0,0 +1,13 @@
+package io.jexxa.jlegmed.plugins.html;
+
+public final class HTMLProperties {
+    public static final String HTTP_PROXY_HOST = "http.proxy.host";
+    public static final String HTTP_PROXY_PORT = "http.proxy.port";
+    public static final String HTTP_PROXY_USERNAME = "http.proxy.username";
+    public static final String HTTP_PROXY_PASSWORD = "http.proxy.password";
+    public static final String HTTP_PROXY_FILE_USERNAME = "http.proxy.file.username";
+    public static final String HTTP_PROXY_FILE_PASSWORD = "http.proxy.file.password";
+
+    private HTMLProperties()
+    {}
+}
--- src/main/java/io/jexxa/jlegmed/plugins/html/producer/HTMLReader.java
+++ src/main/java/io/jexxa/jlegmed/plugins/html/producer/HTMLReader.java
@@ -1,12 +1,24 @@
 package io.jexxa.jlegmed.plugins.html.producer;
 
+import io.jexxa.jlegmed.common.wrapper.utils.properties.Secret;
 import io.jexxa.jlegmed.core.filter.producer.FunctionalProducer;
 import kong.unirest.Unirest;
+import kong.unirest.UnirestInstance;
+
+import java.util.Properties;
+
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_FILE_PASSWORD;
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_FILE_USERNAME;
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_HOST;
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_PASSWORD;
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_PORT;
+import static io.jexxa.jlegmed.plugins.html.HTMLProperties.HTTP_PROXY_USERNAME;
 
 public class HTMLReader<T> extends FunctionalProducer<T> {
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String APPLICATION_TYPE = "application/json";
     private final String url;
+    private UnirestInstance unirestInstance;
 
     public HTMLReader(String url) {
         this.url = url;
@@ -16,14 +28,34 @@ public class HTMLReader<T> extends FunctionalProducer<T> {
     @Override
     public void init() {
         super.init();
-        //TODO Proxy handling
+        unirestInstance = Unirest.spawnInstance();
+        getFilterProperties().ifPresent(this::initUnirest);
     }
 
     @Override
     protected T doProduce() {
-        return Unirest.get(url)
+        return unirestInstance.get(url)
                 .header(CONTENT_TYPE, APPLICATION_TYPE)
                 .asObject(getType()).getBody();
+    }
+
+    @Override
+    public void deInit()
+    {
+        super.deInit();
+        unirestInstance.shutDown();
+        unirestInstance = null;
+    }
+
+    private void initUnirest(Properties properties)
+    {
+        var host = properties.getProperty(HTTP_PROXY_HOST, "");
+        var port = Integer.parseInt(properties.getProperty(HTTP_PROXY_PORT, "0"));
+
+        Secret username = new Secret(properties, HTTP_PROXY_USERNAME, HTTP_PROXY_FILE_USERNAME);
+        Secret password = new Secret(properties, HTTP_PROXY_PASSWORD, HTTP_PROXY_FILE_PASSWORD);
+
+        unirestInstance.config().proxy(host, port, username.getSecret(), password.getSecret());
     }
 
     public static <T> HTMLReader<T> httpURL(String url) {

463c6fd040d6698615e2696c6a062cc138a6d6ee
repplix <116941865+repplix@users.noreply.github.com>
2023-11-05 08:40:05 +0100

    Cleanup: added dedicated test for FunctionalProducer

---
+++ src/test/java/io/jexxa/jlegmed/core/filter/producer/FunctionalProducerTest.java
@@ -0,0 +1,114 @@
+package io.jexxa.jlegmed.core.filter.producer;
+
+import io.jexxa.jlegmed.plugins.generic.pipe.CollectingInputPipe;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+import static io.jexxa.jlegmed.core.filter.FilterProperties.filterPropertiesOf;
+import static io.jexxa.jlegmed.core.filter.producer.FunctionalProducer.producer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FunctionalProducerTest {
+
+    @Test
+    void testSupplyingProducer()
+    {
+        //Arrange
+        CollectingInputPipe<String> inputPipe = new CollectingInputPipe<>();
+        Producer<String> objectUnderTest = producer(() -> "Hello World");
+        objectUnderTest.outputPipe().connectTo(inputPipe);
+
+        objectUnderTest.init();
+
+        //Act
+        objectUnderTest.start();
+
+        //Assert
+        assertEquals(1, inputPipe.getCollectedData().size());
+        assertEquals("Hello World", inputPipe.getCollectedData().get(0));
+    }
+
+    @Test
+    void testFunctionProducer()
+    {
+        //Arrange
+        var propertiesName = "someProperties";
+        CollectingInputPipe<String> inputPipe = new CollectingInputPipe<>();
+        Producer<String> objectUnderTest = producer(filterContext ->
+                "Hello World" + filterContext.filterProperties().orElseThrow().propertiesName()
+        );
+
+        objectUnderTest.useProperties(filterPropertiesOf(propertiesName, new Properties()));
+        objectUnderTest.outputPipe().connectTo(inputPipe);
+
+        //Act
+        objectUnderTest.reachStarted();
+
+        //Assert
+        assertEquals(1, inputPipe.getCollectedData().size());
+        assertEquals("Hello World"+ propertiesName , inputPipe.getCollectedData().get(0));
+    }
+
+    @Test
+    void testBiFunctionProducer()
+    {
+        //Arrange
+        var propertiesName = "someProperties";
+        CollectingInputPipe<String> inputPipe = new CollectingInputPipe<>();
+        Producer<String> objectUnderTest = producer( (filterContext, dataType) ->
+                "Hello World" + filterContext.filterProperties().orElseThrow().propertiesName() + dataType.getSimpleName()
+        );
+
+        objectUnderTest.producingType(String.class);
+        objectUnderTest.useProperties(filterPropertiesOf(propertiesName, new Properties()));
+        objectUnderTest.outputPipe().connectTo(inputPipe);
+
+        //Act
+        objectUnderTest.reachStarted();
+
+        //Assert
+        assertEquals(1, inputPipe.getCollectedData().size());
+        assertEquals("Hello World" + propertiesName + String.class.getSimpleName(), inputPipe.getCollectedData().get(0));
+    }
+
+    @Test
+    void testProduceAgain()
+    {
+        //Arrange
+        CollectingInputPipe<String> inputPipe = new CollectingInputPipe<>();
+        Producer<String> objectUnderTest = producer(filterContext -> {
+            // Here we tell the producer that we must be called again
+            if (!filterContext.processingState().isProcessingAgain()) {
+                filterContext.processingState().processAgain();
+            }
+            return "Hello World";
+        });
+
+        objectUnderTest.outputPipe().connectTo(inputPipe);
+
+        //Act
+        objectUnderTest.reachStarted();
+
+        //Assert
+        assertEquals(2, inputPipe.getCollectedData().size());
+        assertEquals("Hello World", inputPipe.getCollectedData().get(0));
+        assertEquals("Hello World", inputPipe.getCollectedData().get(1));
+    }
+
+    @Test
+    void testFunctionProducerThrowing()
+    {
+        //Arrange - Without properties so that our producer throws an exception
+        CollectingInputPipe<String> inputPipe = new CollectingInputPipe<>();
+        Producer<String> objectUnderTest = producer(filterContext ->
+                "Hello World" + filterContext.filterProperties().orElseThrow().propertiesName()
+        );
+        objectUnderTest.outputPipe().connectTo(inputPipe);
+
+        //Act / Assert
+        assertThrows(NoSuchElementException.class, objectUnderTest::reachStarted);
+    }
+}

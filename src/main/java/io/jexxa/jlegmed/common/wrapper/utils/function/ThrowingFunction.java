67760732e9acda2b4683c49962a2d0c1ea3cb3e8
repplix <116941865+repplix@users.noreply.github.com>
2023-11-07 08:19:48 +0100

    Added functional interfaces that are able to accept throwing methods

---
+++ src/main/java/io/jexxa/jlegmed/common/wrapper/utils/function/ThrowingFunction.java
@@ -0,0 +1,25 @@
+package io.jexxa.jlegmed.common.wrapper.utils.function;
+
+/**
+ * This utility class allows running methods which throw a checked exception inside a java stream
+ * <p>
+ * Example:
+ * <pre>
+ * {@code}
+ * Integer[] values = {1,2,3};
+ * Arrays.stream(values).
+ *   forEach(
+ *    exceptionLogger(value -&gt; Integer.divideUnsigned(value, 0))
+ * );
+ * {@code}
+ * </pre>
+ *
+ *
+ * @param <T> Lambda expression
+ * @param <E> Type of the exception
+ */
+@FunctionalInterface
+public interface ThrowingFunction<T, R, E extends Exception> {
+    R apply(T u) throws E;
+}
+

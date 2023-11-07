40e32bf34d8e727fee0a3f0ffa6f3f22a76db50c
repplix <116941865+repplix@users.noreply.github.com>
2023-11-07 08:21:03 +0100

    Removed javadoc because it does not match

--- src/main/java/io/jexxa/jlegmed/common/wrapper/utils/function/ThrowingBiFunction.java
+++ src/main/java/io/jexxa/jlegmed/common/wrapper/utils/function/ThrowingBiFunction.java
@@ -1,26 +1,8 @@
 package io.jexxa.jlegmed.common.wrapper.utils.function;
 
-/**
- * This utility class allows running methods which throw a checked exception inside a java stream
- * <p>
- * Example:
- * <pre>
- * {@code}
- * Integer[] values = {1,2,3};
- * Arrays.stream(values).
- *   forEach(
- *    exceptionLogger(value -&gt; Integer.divideUnsigned(value, 0))
- * );
- * {@code}
- * </pre>
- *
- *
- * @param <T> Lambda expression
- * @param <E> Type of the exception
- */
+
 @FunctionalInterface
 public interface ThrowingBiFunction<U, V, R, E extends Exception> {
     R apply(U u, V v) throws E;
-
 }
 

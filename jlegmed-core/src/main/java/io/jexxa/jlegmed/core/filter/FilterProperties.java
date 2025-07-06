7b34641ddd40e6464b1ee34a82bc77d7e9a5f801
repplix <116941865+repplix@users.noreply.github.com>
2025-07-06 17:32:45 +0200

    Fix: create a deep copy of given properties. Otherwise, the internal properties could be overwritten if reused in other FilterProperties

--- jlegmed-core/src/main/java/io/jexxa/jlegmed/core/filter/FilterProperties.java
+++ jlegmed-core/src/main/java/io/jexxa/jlegmed/core/filter/FilterProperties.java
@@ -4,9 +4,11 @@ import java.util.Objects;
 import java.util.Properties;
 
 public record FilterProperties(String name, Properties properties) {
-    public FilterProperties {
-        Objects.requireNonNull(name);
+    public FilterProperties(String name, Properties properties) {
+        this.name = Objects.requireNonNull(name);
         Objects.requireNonNull(properties);
+        this.properties = new Properties();
+        this.properties.putAll(properties);
     }
 
     public static FilterProperties filterPropertiesOf(String name, Properties properties) {

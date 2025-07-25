d105e45291af7e41e624d890c5219bf4eebafc18
repplix <116941865+repplix@users.noreply.github.com>
2025-07-25 16:54:33 +0200

    Added annotation for error structures flow graph

---
+++ jlegmed-core/src/main/java/io/jexxa/jlegmed/annotation/FlowError.java
@@ -0,0 +1,19 @@
+package io.jexxa.jlegmed.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Represents data transported in between Filters that is not published.
+ */
+@Target(TYPE)
+@Retention(RUNTIME)
+@Documented
+public @interface FlowError
+{
+
+}

8faf4d63e5629301b236af56f44995d28608e90b
repplix <116941865+repplix@users.noreply.github.com>
2025-07-27 10:59:51 +0200

    Fix: Fixed target type of annotations which must be TYPE

--- jlegmed-core/src/main/java/io/jexxa/jlegmed/annotation/ConsumedMessage.java
+++ jlegmed-core/src/main/java/io/jexxa/jlegmed/annotation/ConsumedMessage.java
@@ -7,14 +7,8 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * Represents a value that must be published e.g., to a legacy system, but no more
- * context is available to use @DomainEvent or @TelemetryData
- */
 @Target(TYPE)
 @Retention(RUNTIME)
 @Documented
-public @interface PublishedMessage
-{
-
+public @interface  ConsumedMessage {
 }
--- jlegmed-core/src/main/java/io/jexxa/jlegmed/annotation/TelemetryData.java
+++ jlegmed-core/src/main/java/io/jexxa/jlegmed/annotation/TelemetryData.java
@@ -4,13 +4,13 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Represents a measured value such as the temperature that is published
  */
-@Target(METHOD)
+@Target(TYPE)
 @Retention(RUNTIME)
 @Documented
 public @interface TelemetryData

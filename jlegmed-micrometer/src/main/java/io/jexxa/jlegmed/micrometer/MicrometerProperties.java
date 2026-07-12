a92940205e5054d2b814e3213d300bdc2c6ac521
repplix <116941865+repplix@users.noreply.github.com>
2026-07-12 12:18:09 +0200

    added new package with micrometer support

---
+++ jlegmed-micrometer/src/main/java/io/jexxa/jlegmed/micrometer/MicrometerProperties.java
@@ -0,0 +1,7 @@
+package io.jexxa.jlegmed.micrometer;
+
+public class MicrometerProperties {
+    /** Define an additional import file that is loaded. The Default value is empty.  */
+    public static final String JLEGMED_PROMETHEUS_PORT =  "jlegmed.prometheus.port";
+
+}
